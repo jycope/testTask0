ec1d2f7a7937a4f365df5ff37940be99e0c01378
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-04 19:06:31 +0500

    Шаблон для создания

---
+++ resources/views/lists/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+{{ Form::model($list, ['route' => 'lists.store', 'class' => 'd-flex justify-content-between flex-column gap-10 mt-4 w-25 m-auto', 'enctype' => 'multipart/form-data']) }}
+<div class="mb-1">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Название</label>
+    <div class="input-form">
+        {{ Form::text('name', null, ['class' => 'form-control']) }}
+    </div>
+</div>
+
+{{ Form::submit('Создать', ['class' => 'form-control mb-4 btn btn-primary']) }}
+{{ Form::close() }}
+
+@endsection
